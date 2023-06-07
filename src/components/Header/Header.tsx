798950efa6eea595eec55f4ca797c1d09d35e492
Suprabh Jain <supjain@deloitte.com>
2023-06-07 10:27:37 +0530

    header change

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,15 +1,14 @@
-import './Header.css';
-import { Link } from 'react-router-dom';
+import "./Header.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-  <div className="header-box">
-    <Link className="home-link" to='/'>
-    Captivating Tales
-    </Link>
-    
-  </div>
+    <div className="header-box">
+      <Link className="home-link" to="">
+        Captivating Tales
+      </Link>
+    </div>
   );
-}
+};
 
 export default Header;
